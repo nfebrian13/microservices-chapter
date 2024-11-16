ca9579d33aa935ca2eaa87e7e572c95c8302b0cb
Nana Febriana <Nana.Febriana@msiglife.co.id>
2024-11-16 20:04:14 +0700

    added config refresh scope for automatic change variable in config

--- customer-service/src/main/java/com/kelaskoding/controller/CustomerControler.java
+++ customer-service/src/main/java/com/kelaskoding/controller/CustomerControler.java
@@ -2,6 +2,7 @@ package com.kelaskoding.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.kelaskoding.service.CustomerService;
 
 @RestController
 @RequestMapping("/api/customers")
+@RefreshScope
 public class CustomerControler {
 
 	@Value("${spring.application.version}")
